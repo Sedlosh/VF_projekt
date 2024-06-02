bb4afe3a47af783375920ccc141129154ba2d9f6
David Sedláček <seddav000@gmail.com>
2024-06-02 19:31:47 +0200

    Add project files.

---
+++ VF_projekt/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace VF_projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.award = new System.Windows.Forms.PictureBox();
+            this.explosion = new System.Windows.Forms.PictureBox();
+            this.AI1 = new System.Windows.Forms.PictureBox();
+            this.AI2 = new System.Windows.Forms.PictureBox();
+            this.floor1 = new System.Windows.Forms.PictureBox();
+            this.floor2 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.scoreBoard = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.award)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Controls.Add(this.award);
+            this.panel1.Controls.Add(this.explosion);
+            this.panel1.Controls.Add(this.AI1);
+            this.panel1.Controls.Add(this.AI2);
+            this.panel1.Controls.Add(this.floor1);
+            this.panel1.Controls.Add(this.floor2);
+            this.panel1.Location = new System.Drawing.Point(12, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(475, 543);
+            this.panel1.TabIndex = 0;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImage = global::VF_projekt.Properties.Resources.pokusOTransparentnost;
+            this.player.Cursor = System.Windows.Forms.Cursors.Default;
+            this.player.Image = global::VF_projekt.Properties.Resources.hazmat;
+            this.player.InitialImage = null;
+            this.player.Location = new System.Drawing.Point(427, 432);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(45, 98);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.player.TabIndex = 3;
+            this.player.TabStop = false;
+            // 
+            // award
+            // 
+            this.award.Image = global::VF_projekt.Properties.Resources.bronze;
+            this.award.Location = new System.Drawing.Point(109, 213);
+            this.award.Name = "award";
+            this.award.Size = new System.Drawing.Size(250, 100);
+            this.award.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.award.TabIndex = 4;
+            this.award.TabStop = false;
+            // 
+            // explosion
+            // 
+            this.explosion.Image = global::VF_projekt.Properties.Resources.explosion;
+            this.explosion.Location = new System.Drawing.Point(100, 383);
+            this.explosion.Name = "explosion";
+            this.explosion.Size = new System.Drawing.Size(50, 50);
+            this.explosion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.explosion.TabIndex = 4;
+            this.explosion.TabStop = false;
+            // 
+            // AI1
+            // 
+            this.AI1.BackColor = System.Drawing.Color.Transparent;
+            this.AI1.BackgroundImage = global::VF_projekt.Properties.Resources.pokusOTransparentnost;
+            this.AI1.Image = global::VF_projekt.Properties.Resources.radiace_final;
+            this.AI1.Location = new System.Drawing.Point(109, 123);
+            this.AI1.Name = "AI1";
+            this.AI1.Size = new System.Drawing.Size(42, 42);
+            this.AI1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.AI1.TabIndex = 4;
+            this.AI1.TabStop = false;
+            this.AI1.Tag = "radiationLeft";
+            // 
+            // AI2
+            // 
+            this.AI2.BackColor = System.Drawing.Color.Transparent;
+            this.AI2.BackgroundImage = global::VF_projekt.Properties.Resources.pokusOTransparentnost;
+            this.AI2.Image = global::VF_projekt.Properties.Resources.radiace_final;
+            this.AI2.Location = new System.Drawing.Point(317, 123);
+            this.AI2.Name = "AI2";
+            this.AI2.Size = new System.Drawing.Size(42, 42);
+            this.AI2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.AI2.TabIndex = 4;
+            this.AI2.TabStop = false;
+            this.AI2.Tag = "radiationRight";
+            // 
+            // floor1
+            // 
+            this.floor1.Image = global::VF_projekt.Properties.Resources.xxxx;
+            this.floor1.Location = new System.Drawing.Point(0, -500);
+            this.floor1.Name = "floor1";
+            this.floor1.Size = new System.Drawing.Size(475, 643);
+            this.floor1.TabIndex = 3;
+            this.floor1.TabStop = false;
+            // 
+            // floor2
+            // 
+            this.floor2.Image = global::VF_projekt.Properties.Resources.xxxx;
+            this.floor2.Location = new System.Drawing.Point(0, 0);
+            this.floor2.Name = "floor2";
+            this.floor2.Size = new System.Drawing.Size(475, 643);
+            this.floor2.TabIndex = 1;
+            this.floor2.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStart.Location = new System.Drawing.Point(199, 586);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(97, 42);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.restart);
+            // 
+            // scoreBoard
+            // 
+            this.scoreBoard.BackColor = System.Drawing.Color.White;
+            this.scoreBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.scoreBoard.Location = new System.Drawing.Point(12, 545);
+            this.scoreBoard.Name = "scoreBoard";
+            this.scoreBoard.Size = new System.Drawing.Size(475, 38);
+            this.scoreBoard.TabIndex = 1;
+            this.scoreBoard.Text = "Score: 0";
+            this.scoreBoard.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.scoreBoard.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(14, 653);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(473, 119);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pohybujte se šipkami vlevo a vpravo.\r\n\r\nVyhýbejte se radiaci a přežijte co nejdél" +
+    "e to zvládnete.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(499, 781);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.scoreBoard);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Hra pro VF";
+            this.TransparencyKey = System.Drawing.Color.Purple;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.award)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label scoreBoard;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox floor1;
+        private System.Windows.Forms.PictureBox AI2;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox AI1;
+        private System.Windows.Forms.PictureBox award;
+        private System.Windows.Forms.PictureBox explosion;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox floor2;
+    }
+}
+
---
+++ VF_projekt/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VF_projekt
+{
+    public partial class Form1 : Form
+    {
+
+        int roadSpeed;
+        int trafficSpeed;
+        int playerSpeed = 12;
+        int score;
+        int aiImage;
+
+        Random rand = new Random();
+        Random radiationPosition = new Random();
+
+        bool goleft, goright;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            ResetGame();
+        }
+      
+        private void positionAI(PictureBox radiation)
+        {
+            radiation.Top = radiationPosition.Next(100, 400) * -1;
+            
+
+            if ((string)radiation.Tag == "radiationLeft")
+            {
+                radiation.Left = radiationPosition.Next(5, 200);
+            }
+            if ((string)radiation.Tag == "radiationRight")
+            {
+                radiation.Left = radiationPosition.Next(245, 422);
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+
+        }
+
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+
+        }
+
+
+        private async void gameTimerEvent(object sender, EventArgs e)
+        {
+
+            scoreBoard.Text = "Score: " + score;
+            score++;
+
+
+            if (goleft == true && player.Left > 5)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goright == true && player.Right < 465)
+            {
+                player.Left += playerSpeed;
+            }
+
+            floor1.Top += roadSpeed;
+            floor2.Top += roadSpeed;
+
+            if (floor2.Top > 519)
+            {
+                floor2.Top = -519;
+            }
+            if (floor1.Top > 519)
+            {
+                floor1.Top = -519;
+            }
+
+            AI1.Top += trafficSpeed;
+            AI2.Top += trafficSpeed;
+
+            if (AI1.Top > 500)
+            {
+                positionAI(AI1);
+            }
+
+            if (AI2.Top > 500)
+            {
+                positionAI(AI2);
+            }
+
+            if (player.Bounds.IntersectsWith(AI1.Bounds) || player.Bounds.IntersectsWith(AI2.Bounds))
+            {
+                gameOver();
+            }
+
+            if (score > 40 && score < 50)
+            {
+                award.Image = Properties.Resources.bronze;
+            }
+
+            if (score > 50 && score < 2000)
+            {
+                award.Image = Properties.Resources.silver;
+                roadSpeed = 20;
+                trafficSpeed = 22;
+            }
+
+            if (score > 2000)
+            {
+                award.Image = Properties.Resources.gold;
+                roadSpeed = 25;
+                trafficSpeed = 27;
+            }
+
+
+        }
+
+        private void gameOver()
+        {
+            playSound();
+            gameTimer.Stop();
+            explosion.Visible = true;
+            player.Controls.Add(explosion);
+            explosion.Location = new Point(-3, 5);
+            explosion.BackColor = Color.Transparent;
+
+            award.Visible = true;
+            award.BringToFront();
+
+            btnStart.Enabled = true;
+
+        }
+
+        private void ResetGame()
+        {
+            btnStart.Enabled = false;
+            explosion.Visible = false;
+            award.Visible = false;
+            goleft = false ;    
+            goright = false ;
+            score = 0;
+            award.Image = Properties.Resources.bronze;
+
+            roadSpeed = 12;
+            trafficSpeed = 15;
+
+            AI1.Top = radiationPosition.Next(200, 500) * -1;
+            AI1.Left = radiationPosition.Next(5, 200);
+
+            AI2.Top = radiationPosition.Next(200, 500) * -1;
+            AI2.Left = radiationPosition.Next(245, 422);
+
+            gameTimer.Start();
+        }
+
+        private void restart(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+
+        private void playSound()
+        {
+            System.Media.SoundPlayer playExplosion = new System.Media.SoundPlayer(Properties.Resources.radiation);  
+            playExplosion.Play();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
